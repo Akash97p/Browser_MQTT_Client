c4feed8af618bed86d738b340645bb484bd0ea98
Akash97p <akash.anjanam@gmail.com>
2020-09-27 22:50:22 +0530

    pub ok

--- scripts/main.js
+++ scripts/main.js
@@ -28,17 +28,41 @@ client.on('error', function (err) {
 })
 
 // -----------------------------------------------------------------------------------
-
+const conf_btn = document.getElementById('conf_btn');
 const pub_btn = document.getElementById('pub_btn');
 const sub_btn = document.getElementById('sub_btn');
 const pub_debug = document.getElementById('pub_debug')
 const sub_debug = document.getElementById('sub_debug')
+const conf_debug = document.getElementById('conf_debug')
+
+var sub_topic = 'topic1';
+var prev_sub_topic ;
 
 pub_btn.addEventListener('click' , function publish(){
     x = document.getElementById("msg").value;
-    y = client.publish('presence', x)
-    pub_debug.textContent = 'Published' ;
-    console.log('Published'+y);  
+    y = document.getElementById("pub_topic").value;
+    if(y.length != 0 ){
+        if(x.length != 0 ){
+        client.publish(y, x)
+        pub_debug.textContent = 'Published' ;
+        console.log('Published');
+        }
+        else{
+            pub_debug.textContent = 'Message can\'t be blank' ;
+            console.log('Message can\'t be blank');
+        }
+    }
+    else{
+        if(x.length != 0 ){
+        pub_debug.textContent = 'Topic can\'t be blank' ;
+        console.log('Topic can\'t be blank');
+        }
+        else{
+            pub_debug.textContent = 'Topic and Message can\'t be blank' ;
+            console.log('Topic and Message can\'t be blank');
+        }
+    }
+  
 } );
 
 sub_btn.addEventListener('click' , function subscribe(){
@@ -49,9 +73,9 @@ sub_btn.addEventListener('click' , function subscribe(){
 
 client.on('connect', function () {
     console.log('Connected to '+ host);
-    client.subscribe('presence', function (err) {
+    client.subscribe(sub_topic, function (err) {
       if (!err) {
-        client.publish('presence', 'Hello mqtt')
+        // client.publish('ack', 'Hello mqtt')
       }
     })
   })
