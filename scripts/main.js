6101e430f3c69e75b3f9028ba9c17aa87aed3e40
Akash97p <akash.anjanam@gmail.com>
2020-10-07 13:04:32 +0530

    new debug window

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,69 @@
-var conf_btn = document.getElementById('conf_btn');
-var pub_btn = document.getElementById('pub_btn');
-var sub_btn = document.getElementById('sub_btn');
-var un_sub_btn = document.getElementById('un_sub_btn');
-var conf_exp_btn = document.getElementById('conf_exp');
-var con_discon = document.getElementById('con_status_div');
-var sub_debug = document.getElementById('sub_debug_window');
-var con_status = document.getElementById('con_status');
-var debug_window = document.getElementById('debug_window');
+var configBtn = document.getElementById('conf_btn');
+var pubBtn = document.getElementById('pub_btn');
+var subBtn = document.getElementById('sub_btn');
+var unSubBtn = document.getElementById('un_sub_btn');
+var confExpBtn = document.getElementById('conf_exp');
+var subDebug = document.getElementById('sub_debug_window');
+var conStatus = document.getElementById('con_status');
+var debugWindow = document.getElementById('debug_window');
 
-var new_sec = new Boolean(true);
+var subscibed = new Boolean(false);
 var i = new Boolean(true);
 var k =2;
-var prevMsg ='';
-let dw = ['dw1','dw2','dw3','dw4','dw5','dw6'];
+var subMsgArray = [];
+let csvContent = "data:text/csv;charset=utf-8,";
+
+function conf(x, y) {
+    window.clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
+    if (y.length != 0) {
+        clientId = y;
+    }
+
+    window.host = x;
+    window.activeTopic = '0';
+    var options = {
+        keepalive: 30,
+        clientId: clientId,
+        protocolId: 'MQTT',
+        protocolVersion: 4,
+        clean: true,
+        reconnectPeriod: 1000,
+        connectTimeout: 30 * 1000,
+        will: {
+            topic: 'WillMsg',
+            payload: 'Connection Closed abnormally..!',
+            qos: 0,
+            retain: false
+        },
+        rejectUnauthorized: false
+    }
+
+    window.client = mqtt.connect(host, options)
+    client.on('error', function(err) { 
+        console.log('bla + ' + err)
+        printToDebugWindow(err);
+        client.end()
+    })
+
+    client.on('connect', function() {
+            if (client.connected) {
+                console.log('Connected to ' + host + '  | Client ID' + clientId);
+                conStatus.textContent = 'Connected';
+                document.getElementsByClassName('con_status_div')[0].style.backgroundColor = '#5bce74';
+                printToDebugWindow('Connected to ' + host + '  | Client ID : ' + clientId);
+                expand(true);
+                connection_stat();
+            } else {
+                conStatus.textContent = 'Disconnected';
+                conStatus.style.backgroundColor =  '#e77a7a';
+            }
+    })
+
+    client.on('message', function(topic, message) {
+        console.log('Received "' + message.toString() + '" from topic "' + topic.toString() + '"');
+        printSubmsg(message.toString());
+    })
+}
 
 function expand(cm) {
     if(cm){
@@ -43,34 +94,58 @@ function time(){
 
 function connection_stat(){
     document.getElementsByClassName('con_status_div')[0].style.backgroundColor = client.connected ?  '#5bce74' :'#e77a7a' ;
-    con_status.textContent = client.connected ? 'Connected' : 'Disconected';
+    conStatus.textContent = client.connected ? 'Connected' : 'Disconected';
     if(!client.connected) {
+        alert( 'Disconnected from ' + host)
+        printToDebugWindow('Disconnected from ' + host);
         expand(false);
     }
+    else{
     setTimeout(connection_stat,1000);
+    }
 }
 
-function write_debug_window(msg){
-    if(msg === document.getElementById(dw[0]).innerText || k-1+ " " +msg === document.getElementById(dw[0]).innerText){
-        document.getElementById(dw[0]).textContent = k+ " " +msg;
+function printToDebugWindow(msg){
+    if(msg === document.getElementsByClassName('msg')[0].innerText || k-1+ " " +msg === document.getElementsByClassName('msg')[0].innerText){
+        var template = [];
+        template.push(
+            '<div style="color:#056676;font-family: "Baloo Tammudu 2", sans-serif;">',
+                '<span>' + time()+ '</span>',
+            '</div>',
+            '<div class="msg" style="font-family: "Raleway", sans-serif;">',
+            '<span style="color:red"><b>' + k + '</b></span> <span><b>' + msg + '</b></span>',
+            '</div>'
+        );
+        document.getElementsByClassName('msg_div')[0].innerHTML = template.join('');;
         k=k+1;
+        console.log('inside is prev msg')
     }
     else{
-        for (let index = dw.length; index >=2; index--) {
-            document.getElementById(dw[index-1]).textContent = document.getElementById(dw[index-2]).innerText;
-        }
-    document.getElementById(dw[0]).textContent = msg
-    k=2;
+        console.log(document.getElementsByClassName('msg')[0].innerText);
+        var diva = document.createElement('div');
+        diva.className = 'msg_div';
+        var template = [];
+        template.push(
+            '<div style="color:#056676;font-family: "Baloo Tammudu 2", sans-serif;">',
+                '<span>' + time()+ '</span>',
+            '</div>',
+            '<div class="msg" style="font-family: "Raleway", sans-serif;">',
+                '<span><b>' + msg + '</b></span>',
+            '</div>'
+        );
+        diva.innerHTML = template.join('');
+        debugWindow.prepend(diva);
+        k=2;
     }
+
+
 }
 
-function write_sub_debug_window(msg){
+function printSubmsg(msg){
+    subMsgArray.push(time()+','+msg);
+    console.log(subMsgArray);
     var diva = document.createElement('div');
     diva.className = 'msg_div';
-    // var pElem = document.createElement('p');
-    // pElem.innerHTML = msg;
-    // diva.append(pElem);
-    // sub_debug.prepend(diva);
     var template = [];
     template.push(
         '<div style="color:#056676;font-family: "Baloo Tammudu 2", sans-serif;">',
@@ -81,16 +156,66 @@ function write_sub_debug_window(msg){
         '</div>'
     );
     diva.innerHTML = template.join('');
-    sub_debug.prepend(diva);
+    subDebug.prepend(diva);
 }
 
-conf_exp_btn.addEventListener('click',function exp(){
+function unSubscribe() {
+    subMsgArray.forEach( function (line){
+        csvContent += line + "\r\n";
+    });
+    var encodedUri = encodeURI(csvContent);
+    window.open(encodedUri);
+    if(subscibed){
+        client.unsubscribe(activeTopic, function(err) {
+            if (!err) {
+                subscibed = false;
+                subDebug.textContent = ('');
+                console.log('Unsubscribed from : ' + activeTopic);
+                document.getElementById('activeTopic').textContent = '';
+                printToDebugWindow('Unsubscribed from : ' + activeTopic);
+                document.getElementsByClassName('unsub_btn')[0].style.display ="none";
+                activeTopic = '';
+            }
+        });
+    }
+}
+
+function subscribe() {
+    const newTopic = document.getElementById("sub_topic").value;
+    if (newTopic.length != 0) {
+        if(newTopic===activeTopic){
+            printToDebugWindow('Already subscribed to : ' + activeTopic);
+        }
+        else{
+             if(subscibed === true){
+                unSubscribe();
+            }
+            client.subscribe(newTopic, function(err) {
+                if (!err) {
+                    subscibed = true;
+                    activeTopic = newTopic;
+                    console.log('Subscribed to : ' + activeTopic);
+                    printToDebugWindow('Subscribed to : ' + activeTopic);
+                    document.getElementsByClassName('unsub_btn')[0].style.display ="inline";
+                    document.getElementById('activeTopic').textContent = '    [ Topic : '+activeTopic +' ]';
+                }
+            });
+        } 
+    }
+    else {
+            printToDebugWindow('Topic can\'t be blank');
+            console.log('Topic can\'t be blank');
+    }
+
+}
+
+confExpBtn.addEventListener('click',function exp(){
     console.log('exp btn pressed');
     expand(i);
     i = !i;
 });
 
-conf_btn.addEventListener('click', function config() {
+configBtn.addEventListener('click', function config() {
     brocker = document.getElementById("host").value;
     port = document.getElementById("port").value;
     clientId = document.getElementById("clientId").value;
@@ -98,138 +223,37 @@ conf_btn.addEventListener('click', function config() {
         var host = 'ws://' + brocker + ':' + port
         conf(host, clientId);
     } else {
-        write_debug_window('Values can\'t be blank');
+        printToDebugWindow('Values can\'t be blank');
         console.log('Values can\'t be blank');
     }
 });
 
-pub_btn.addEventListener('click', function publish() {
+pubBtn.addEventListener('click', function publish() {
     x = document.getElementById("msg").value;
     y = document.getElementById("pub_topic").value;
     if (y.length != 0) {
         if (x.length != 0) {
             client.publish(y, x)
-            write_debug_window('Published "' + x + '" to topic "' + y);
+            printToDebugWindow('Published "' + x + '" to topic "' + y);
             console.log('Published "' + x + '"to topic "' + y + '"');
         } else {
-            write_debug_window('Message can\'t be blank');
+            printToDebugWindow('Message can\'t be blank');
             console.log('Message can\'t be blank');
         }
     } else {
         if (x.length != 0) {
-            write_debug_window('Topic can\'t be blank');
+            printToDebugWindow('Topic can\'t be blank');
             console.log('Topic can\'t be blank');
         } else {
-            write_debug_window('Topic and Message can\'t be blank');
+            printToDebugWindow('Topic and Message can\'t be blank');
             console.log('Topic and Message can\'t be blank');
         }
     }
 
 });
 
-sub_btn.addEventListener('click', function subscribe() {
-    x = document.getElementById("sub_topic").value;
-    if (x.length != 0) {
-        if(x===sub_topic && !new_sec){
-            write_debug_window('Already subscribed to : ' + sub_topic);
-        }
-        else{
-        if(!new_sec){
-            client.unsubscribe(sub_topic, function(err) {
-                if (!err) {
-                    console.log('Unsubscribed from : ' + sub_topic);
-                    write_debug_window('Unsubscribed from : ' + sub_topic);
-                    document.getElementById('active_topic').textContent = '';
-                    document.getElementsByClassName('unsub_btn')[0].style.display ="none";
-            }
-            })
-        }
-        client.subscribe(x, function(err) {
-            if (!err) {
-                write_debug_window('Subscribed to : ' + x);
-                sub_topic = x;
-                document.getElementById('active_topic').textContent = '    [ Topic : '+x +' ]';
-                console.log('Subscribed to : ' + x);
-                new_sec = false;
-                document.getElementsByClassName('unsub_btn')[0].style.display ="inline";
-            }
-        })
-    } 
-}else {
-        write_debug_window('Topic can\'t be blank');
-        console.log('Topic can\'t be blank');
-    }
+subBtn.addEventListener('click', subscribe);
 
-});
-
-un_sub_btn.addEventListener('click', function subscribe() {
-    if(!new_sec){
-        client.unsubscribe(sub_topic, function(err) {
-            if (!err) {
-                console.log('Unsubscribed from : ' + sub_topic);
-                write_debug_window('Unsubscribed from : ' + sub_topic);
-                    document.getElementById('active_topic').textContent = '';
-                    sub_debug.textContent = ('');
-                    document.getElementsByClassName('unsub_btn')[0].style.display ="none";
-            }
-        })
-        new_sec = true;
-    }
-});
-
-// --------------------------------- MQTT Conf ----------------------------------------
-function conf(x, y) {
-    window.clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
-    if (y.length != 0) {
-        clientId = y;
-    }
-
-    window.host = x;
-    window.sub_topic = 'default_topic';
-    var options = {
-        keepalive: 30,
-        clientId: clientId,
-        protocolId: 'MQTT',
-        protocolVersion: 4,
-        clean: true,
-        reconnectPeriod: 1000,
-        connectTimeout: 30 * 1000,
-        will: {
-            topic: 'WillMsg',
-            payload: 'Connection Closed abnormally..!',
-            qos: 0,
-            retain: false
-        },
-        rejectUnauthorized: false
-    }
-
-    window.client = mqtt.connect(host, options)
-    client.on('error', function(err) { 
-        console.log('bla + ' + err)
-        write_debug_window(err);
-        client.end()
-    })
-
-    client.on('connect', function() {
-            if (client.connected) {
-                console.log('Connected to ' + host + '  | Client ID' + clientId);
-                con_status.textContent = 'Connected';
-                document.getElementsByClassName('con_status_div')[0].style.backgroundColor = '#5bce74';
-                write_debug_window('Connected to ' + host + '  | Client ID : ' + clientId);
-                expand(true);
-                connection_stat();
-            } else {
-                con_status.textContent = 'Disconnected';
-                con_status.style.backgroundColor =  '#e77a7a';
-            }
-    })
-
-    client.on('message', function(topic, message) {
-        console.log('Received "' + message.toString() + '" from topic "' + topic.toString() + '"')
-        // sub_debug.textContent = 'Received "' + message.toString() + '" from topic "' + topic.toString() + '"';
-        write_sub_debug_window(message.toString());
-    })
-}
-// -----------------------------------------------------------------------------------
+unSubBtn.addEventListener('click', unSubscribe);
 
 conf('ws://localhost:9001','');
